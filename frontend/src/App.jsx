1b1b6b1e79c1841e982b0af7a22eb9dccb5401ac
hammadurrehman01 <hammadurrehman@techlabs.cc>
2024-07-14 20:06:24 +0500

    fix: changes merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Stack } from "@mui/material"
 import Home from "./pages/HomeData"
+import { Link } from "react-router-dom"
 
 const App = () => {
   return (
     <Stack>
       <Home />
+      <Link to={"/signup"}>Signup</Link>
     </Stack>
   )
 }
